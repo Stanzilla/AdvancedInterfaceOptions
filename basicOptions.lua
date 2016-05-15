a012dfaa9529c000be9b03c6fce23236edebf634
semlar <teslagraafs@gmail.com>
2016-05-15 13:41:11 -0400

    weird bug for stanzilla to fix

--- basicOptions.lua
+++ basicOptions.lua
@@ -8,11 +8,26 @@ AIO:Hide()
 AIO:SetAllPoints()
 AIO.name = addonName
 
-local function newCheckbox(label, description, onClick)
+AIO.okay = function()
+	print(GetCVarInfo('unitnameown') == '1' and 'Enabled after Ok' or 'Disabled after Ok')
+end
+AIO.cancel = function()
+	print(GetCVarInfo('unitnameown') == '1' and 'Enabled after Cancel' or 'Disabled after Cancel')
+end
+--local function newCheckbox(label, description, onClick)
+local function newCheckbox(cvar)
+	local cvarTable = addon.hiddenOptions[cvar]
+	local label = _G[cvarTable['prettyName']] or cvar
+	local description = _G[cvarTable['description']] or 'No description'
 	local check = CreateFrame("CheckButton", "AIOCheck" .. label, AIO, "InterfaceOptionsCheckButtonTemplate")
+	check:SetScript('OnShow', function(self)
+		self:SetChecked(GetCVarInfo(cvar) == '1')
+	end)
 	check:SetScript("OnClick", function(self)
 		PlaySound(self:GetChecked() and "igMainMenuOptionCheckBoxOn" or "igMainMenuOptionCheckBoxOff")
-		onClick(self, self:GetChecked() and true or false)
+		SetCVar(cvar, self:GetChecked() and '1' or '0')
+		print(GetCVarInfo(cvar) == '1' and 'Enabled after OnClick' or 'Disabled after OnClick')
+		--onClick(self, self:GetChecked() and true or false)
 	end)
 	check.label = _G[check:GetName() .. "Text"]
 	check.label:SetText(label)
@@ -34,47 +49,15 @@ subText:SetPoint('TOPLEFT', title, 'BOTTOMLEFT', 0, -8)
 subText:SetPoint('RIGHT', -32, 0)
 subText:SetText('These options allow you to toggle various options that have been removed from the game in Legion.')
 
-local playerName = newCheckbox(
+local playerName = newCheckbox('UnitNameOwn')
+--[[
 	"Your Own Name", -- TODO Get prettyName from the hiddenOptions table
 	"UNIT_NAME_OWN",
 	function(self, value) addon.hiddenOptions["UNIT_NAME_OWN"] = value end)
+	
 playerName:SetChecked(true) -- TODO
+--]]
 playerName:SetPoint("TOPLEFT", title, "BOTTOMLEFT", -2, -16)
 
 
 InterfaceOptions_AddCategory(AIO, addonName)
-
---[[
-local info = {}
-local fontSizeDropdown = CreateFrame("Frame", "BugSackFontSize", AIO, "UIDropDownMenuTemplate")
-fontSizeDropdown:SetPoint("TOPLEFT", "BOTTOMLEFT", -15, -10)
-fontSizeDropdown.initialize = function()
-	wipe(info)
-	local fonts = {"GameFontHighlightSmall", "GameFontHighlight", "GameFontHighlightMedium", "GameFontHighlightLarge"}
-	local names = {L["Small"], L["Medium"], L["Large"], L["X-Large"]}
-	for i, font in next, fonts do
-		info.text = names[i]
-		info.value = font
-		info.func = function(self)
-
-		end
-		info.checked = font == addon.db.fontSize
-		UIDropDownMenu_AddButton(info)
-	end
-end
-
-
-local reset = CreateFrame("Button", "AIOResetButton", AIO, "UIPanelButtonTemplate")
-reset:SetText(L["Reset to default"])
-reset:SetWidth(177)
-reset:SetHeight(24)
-reset:SetPoint("TOPLEFT", fontSizeDropdown, "BOTTOMLEFT", 17, -25)
-reset:SetScript("OnClick", function()
-	addon:Reset()
-end)
-reset.tooltipText = L["Reset Advanced Interface Options to Blizzard defaults"]
-reset.newbieText = L.wipeDesc
-
-AIO:SetScript("OnShow", nil)
-end)
-]]--
