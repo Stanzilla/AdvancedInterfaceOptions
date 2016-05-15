93f35d8b3a1dcaf6a155cc22ac460b8faba5f328
Benjamin Staneck <staneck@gmail.com>
2016-05-15 20:26:03 +0200

    add most missing cvars to basicoptions

--- basicOptions.lua
+++ basicOptions.lua
@@ -1,20 +1,13 @@
 local addonName, addon = ...
 local _G = _G
 
--- GLOBALS: ListFrame GameTooltip SLASH_AIO1 InterfaceOptionsFrame_OpenToCategory
+-- GLOBALS: GameTooltip InterfaceOptionsFrame_OpenToCategory
 
 local AIO = CreateFrame('Frame', nil, InterfaceOptionsFramePanelContainer)
 AIO:Hide()
 AIO:SetAllPoints()
 AIO.name = addonName
 
-AIO.okay = function()
-	print(GetCVarInfo('unitnameown') == '1' and 'Enabled after Ok' or 'Disabled after Ok')
-end
-AIO.cancel = function()
-	print(GetCVarInfo('unitnameown') == '1' and 'Enabled after Cancel' or 'Disabled after Cancel')
-end
---local function newCheckbox(label, description, onClick)
 local function newCheckbox(cvar)
 	local cvarTable = addon.hiddenOptions[cvar]
 	local label = _G[cvarTable['prettyName']] or cvar
@@ -27,7 +20,6 @@ local function newCheckbox(cvar)
 		PlaySound(self:GetChecked() and "igMainMenuOptionCheckBoxOn" or "igMainMenuOptionCheckBoxOff")
 		SetCVar(cvar, self:GetChecked() and '1' or '0')
 		print(GetCVarInfo(cvar) == '1' and 'Enabled after OnClick' or 'Disabled after OnClick')
-		--onClick(self, self:GetChecked() and true or false)
 	end)
 	check.label = _G[check:GetName() .. "Text"]
 	check.label:SetText(label)
@@ -49,15 +41,27 @@ subText:SetPoint('TOPLEFT', title, 'BOTTOMLEFT', 0, -8)
 subText:SetPoint('RIGHT', -32, 0)
 subText:SetText('These options allow you to toggle various options that have been removed from the game in Legion.')
 
-local playerName = newCheckbox('UnitNameOwn')
---[[
-	"Your Own Name", -- TODO Get prettyName from the hiddenOptions table
-	"UNIT_NAME_OWN",
-	function(self, value) addon.hiddenOptions["UNIT_NAME_OWN"] = value end)
-	
-playerName:SetChecked(true) -- TODO
---]]
-playerName:SetPoint("TOPLEFT", title, "BOTTOMLEFT", -2, -16)
+local playerTitles = newCheckbox('UnitNamePlayerPVPTitle')
+local playerGuilds = newCheckbox('UnitNamePlayerGuild')
+local playerGuildTitles = newCheckbox('UnitNameGuildTitle')
+local stopAutoAttack = newCheckbox('stopAutoAttackOnTargetChange')
+local reverseCleanupBags = newCheckbox('reverseCleanupBags')
+local attackOnAssist = newCheckbox('assistAttack')
+local autoSelfCast = newCheckbox('autoSelfCast')
+local castOnKeyDown = newCheckbox('ActionButtonUseKeyDown')
+local fadeMap = newCheckbox('mapFade')
+local chatDelay = newCheckbox('removeChatDelay')
+local secureToggle = newCheckbox('secureAbilityToggle')
+local luaErrors = newCheckbox('scriptErrors')
+local lootUnderMouse = newCheckbox('lootUnderMouse')
+local targetDebuffFilter = newCheckbox('noBuffDebuffFilterOnTarget')
+
+playerTitles:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -4)
+playerGuilds:SetPoint("TOPLEFT", playerTitles, "BOTTOMLEFT", 0, -4)
+playerGuildTitles:SetPoint("TOPLEFT", playerGuilds, "BOTTOMLEFT", 0, -4)
+stopAutoAttack:SetPoint("TOPLEFT", playerGuildTitles, "BOTTOMLEFT", 0, -4)
+
+-- TODO reducedLagTolerance maxSpellStartRecoveryOffset
 
 
 InterfaceOptions_AddCategory(AIO, addonName)
